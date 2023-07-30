c206c6d15386e524c122c153bab51247323a33d0
Samir Vergara V <svergara3@misena.edu.co>
2023-07-30 00:42:34 -0500

    inicial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+
+<img alt="Curso de Node.js desde cero y práctico" src="https://github-production-user-asset-6210df.s3.amazonaws.com/1561955/254806429-8ff74316-d49e-4358-8b1e-07d7b5a64ed4.jpeg" width="500">
+
+# Curso de Node.js desde cero ❇️
+
+Curso para aprender **Node** de forma práctica
+**[Todos los jueves a las 18PM 🇪🇸 en Twitch](https://twitch.tv/midudev)**
+</div>
+
+## 📹 Videos con las clases
+
+- 01: [Introducción a Node.js y módulos](https://www.youtube.com/watch?v=yB4n_K7dZV8)
+
+## Horario por países:
+
+- 18H 🇪🇸
+- 17H 🇮🇨
+- 13H 🇺🇾 🇦🇷 🇨🇱
+- 12H 🇵🇾 🇹🇹 🇧🇴 🇻🇪 🇩🇴
+- 11H 🇨🇴 🇵🇪 🇪🇨 🇵🇦
+- 10H 🇲🇽 🇨🇷 🇳🇮 🇸🇻 🇭🇳
---
+++ clase-2/3.express.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const ditto = require('./pokemon/ditto.json')
+
+const PORT = process.env.PORT ?? 1234
+
+const app = express()
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+/* todo el middleware fue reemplazado por: app.use(express.json())
+app.use((req, res, next) => {
+  console.log('Mi primer Middleware');
+  if (req.method !== 'POST') return next()
+  if (req.headers['content-type'] !== 'application/json') return next()
+
+  // solo llegan request que son POST y que tienen el header Content-Type: application/json
+  let body = ''
+   // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+  data.timestamp = Date.now()
+     // mutar la request y meter la información en el req.body
+     req.body = data
+    next()
+  })
+})
+*/
+
+app.get('/', (req, res) => {
+  res.json({message: 'Hello World desde SamirV2!'})
+})
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
+})
+
+app.post('/pokemon', (req, res) => {
+  res.status(201).json(req.body)
+})
+
+// la última a la que va a llegar
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
